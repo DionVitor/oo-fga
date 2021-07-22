e1faee71d62e50eb59ac2f550ad0cd46ae5ac47a
Dion Vitor do Prado <71671413+DionVitor@users.noreply.github.com>
2021-07-21 22:16:54 -0300

    Passos iniciais com o Java.

---
+++ src/Introdução/Conditionals.java
@@ -0,0 +1,13 @@
+package Introdução;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int index = 11;
+
+        if(index == 10) {
+            System.out.println(index);
+        } else {
+          System.out.println(index + 1);
+        }
+    }
+}
---
+++ src/Introdução/Loops.java
@@ -0,0 +1,19 @@
+package Introdução;
+
+public class Loops {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30};
+
+        // for loop common
+        for (int index=0; index < numbers.length; index++) {
+            System.out.println(numbers[index]);
+        }
+
+        System.out.println();
+
+        // for each loop - iterable
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
