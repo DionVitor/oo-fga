abca991ac920cb52d0e2736dd3e1a1f5ee9575b2
dionvitor <dionvictor11@gmail.com>
2021-09-10 10:18:48 -0300

    Adicionado classe Cardápio.

---
+++ src/tp3/Cardapio.java
@@ -0,0 +1,26 @@
+package tp3;
+
+import tp3.Produto;
+import tp3.CRUD;
+
+public class Cardapio extends CRUD {
+    private final String tipo;
+    private final Produto[] produtos;
+
+    public Cardapio(String tipo, Produto[] produtos) {
+        this.tipo = tipo;
+        this.produtos = produtos;
+    }
+
+    public boolean set() {
+        return true;
+    }
+
+    public boolean edit() {
+        return false;
+    }
+
+    public boolean delete() {
+        return false;
+    }
+}
