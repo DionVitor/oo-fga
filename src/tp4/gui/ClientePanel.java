1a031a0f34be69756620d2def40165c2ad1dbef1
dionvitor <dionvictor11@gmail.com>
2021-09-28 18:34:30 -0300

    Adicionado widgets da tela de cadastro de clientes.

--- src/tp4/gui/ClientePanel.java
+++ src/tp4/gui/ClientePanel.java
@@ -9,14 +9,14 @@ public class ClientePanel extends JPanel {
         super(false);
 
         // Break line
-        this.add(Box.createRigidArea(new Dimension(0, 30)));
+        this.add(Box.createRigidArea(new Dimension(1000, 8)));
 
         // Title
         JLabel filler = new JLabel("Cliente");
         this.add(filler);
 
         // Break line
-        this.add(Box.createRigidArea(new Dimension(0, 30)));
+        this.add(Box.createRigidArea(new Dimension(1000, 3)));
 
         // Main TabbedPane
         JTabbedPane tabbedPane = new JTabbedPane();
@@ -30,6 +30,47 @@ public class ClientePanel extends JPanel {
         tabbedPane.addTab("Adicionar cliente", icon, createClientPane);
         tabbedPane.addTab("Editar cliente", icon, editClientPane);
         tabbedPane.addTab("Listar clientes", icon, listClientsPane);
+
+        // Create client pane - title
+        createClientPane.add(Box.createRigidArea(new Dimension(1000, 10)));
+        createClientPane.add(new JLabel("Adicionar cliente"));
+        createClientPane.add(Box.createRigidArea(new Dimension(1000, 20)));
+
+        // Create cliente pane - inputs
+        JLabel nameLabel = new JLabel("Nome:");
+        nameLabel.setPreferredSize(new Dimension(100, 30));
+        JTextField nameInput = new JTextField();
+        nameInput.setPreferredSize(new Dimension(700, 30));
+        JLabel addressLabel = new JLabel("Endereço:");
+        addressLabel.setPreferredSize(new Dimension(100, 30));
+        JTextField addressInput = new JTextField();
+        addressInput.setPreferredSize(new Dimension(700, 30));
+        JLabel phoneLabel = new JLabel("Telefone:");
+        phoneLabel.setPreferredSize(new Dimension(100, 30));
+        JTextField phoneInput = new JTextField();
+        phoneInput.setPreferredSize(new Dimension(700, 30));
+        JLabel paymentLabel = new JLabel("Pagamento:");
+        paymentLabel.setPreferredSize(new Dimension(100, 30));
+        JTextField paymentInput = new JTextField();
+        paymentInput.setPreferredSize(new Dimension(700, 30));
+
+        createClientPane.add(nameLabel);
+        createClientPane.add(nameInput);
+        createClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
+        createClientPane.add(addressLabel);
+        createClientPane.add(addressInput);
+        createClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
+        createClientPane.add(phoneLabel);
+        createClientPane.add(phoneInput);
+        createClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
+        createClientPane.add(paymentLabel);
+        createClientPane.add(paymentInput);
+
+        // Create cliente pane - button
+        createClientPane.add(Box.createRigidArea(new Dimension(1000, 80)));
+        JButton saveButton = new JButton("Adicionar");
+        saveButton.setPreferredSize(new Dimension(200, 30));
+        createClientPane.add(saveButton);
 
         this.add(tabbedPane);
     }
