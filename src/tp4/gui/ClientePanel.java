ddbccfe251ba2537493a1e828480aa0c23c0d42f
dionvitor <dionvictor11@gmail.com>
2021-09-28 16:37:38 -0300

    Adicionado estrutura inicial do painel de clientes.

--- src/tp4/gui/ClientePanel.java
+++ src/tp4/gui/ClientePanel.java
@@ -1,12 +1,36 @@
 package tp4.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 public class ClientePanel extends JPanel {
     public ClientePanel() {
         super(false);
+
+        // Break line
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+
+        // Title
         JLabel filler = new JLabel("Cliente");
         this.add(filler);
+
+        // Break line
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+
+        // Main TabbedPane
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.setPreferredSize(new Dimension(950, 480));
+
+        // Tabs
+        JPanel createClientPane = new JPanel();
+        JPanel editClientPane = new JPanel();
+        JPanel listClientsPane = new JPanel();
+        Icon icon = new ImageIcon();
+        tabbedPane.addTab("Adicionar cliente", icon, createClientPane);
+        tabbedPane.addTab("Editar cliente", icon, editClientPane);
+        tabbedPane.addTab("Listar clientes", icon, listClientsPane);
+
+        this.add(tabbedPane);
     }
 }
