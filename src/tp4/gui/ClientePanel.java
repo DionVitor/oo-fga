e0d3f1a6ce848c6cdd0057b949c49699cba727cc
dionvitor <dionvictor11@gmail.com>
2021-09-28 22:24:22 -0300

    Adicionado opções de remoção de clientes.

--- src/tp4/gui/ClientePanel.java
+++ src/tp4/gui/ClientePanel.java
@@ -8,14 +8,10 @@ public class ClientePanel extends JPanel {
     public ClientePanel() {
         super(false);
 
-        // Break line
-        this.add(Box.createRigidArea(new Dimension(1000, 8)));
-
         // Title
+        this.add(Box.createRigidArea(new Dimension(1000, 8)));
         JLabel filler = new JLabel("Cliente");
         this.add(filler);
-
-        // Break line
         this.add(Box.createRigidArea(new Dimension(1000, 3)));
 
         // Main TabbedPane
@@ -23,20 +19,20 @@ public class ClientePanel extends JPanel {
         tabbedPane.setPreferredSize(new Dimension(950, 480));
 
         // Tabs
-        JPanel createClientPane = new JPanel();
-        JPanel editClientPane = new JPanel();
-        JPanel listClientsPane = new JPanel();
+        JPanel createClientPanel = new JPanel();
+        JPanel editClientPanel = new JPanel();
+        JPanel listClientsPanel = new JPanel();
         Icon icon = new ImageIcon();
-        tabbedPane.addTab("Adicionar cliente", icon, createClientPane);
-        tabbedPane.addTab("Editar cliente", icon, editClientPane);
-        tabbedPane.addTab("Listar clientes", icon, listClientsPane);
+        tabbedPane.addTab("Adicionar cliente", icon, createClientPanel);
+        tabbedPane.addTab("Editar cliente", icon, editClientPanel);
+        tabbedPane.addTab("Listar clientes", icon, listClientsPanel);
 
-        // Create client pane - title
-        createClientPane.add(Box.createRigidArea(new Dimension(1000, 10)));
-        createClientPane.add(new JLabel("Adicionar cliente"));
-        createClientPane.add(Box.createRigidArea(new Dimension(1000, 20)));
+        // Create client panel - title
+        createClientPanel.add(Box.createRigidArea(new Dimension(1000, 10)));
+        createClientPanel.add(new JLabel("Adicionar cliente"));
+        createClientPanel.add(Box.createRigidArea(new Dimension(1000, 20)));
 
-        // Create client pane - inputs
+        // Create client panel - inputs
         JLabel nameLabel = new JLabel("Nome:");
         nameLabel.setPreferredSize(new Dimension(100, 30));
         JTextField nameInput = new JTextField();
@@ -54,40 +50,40 @@ public class ClientePanel extends JPanel {
         JTextField paymentInput = new JTextField();
         paymentInput.setPreferredSize(new Dimension(700, 30));
 
-        createClientPane.add(nameLabel);
-        createClientPane.add(nameInput);
-        createClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
-        createClientPane.add(addressLabel);
-        createClientPane.add(addressInput);
-        createClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
-        createClientPane.add(phoneLabel);
-        createClientPane.add(phoneInput);
-        createClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
-        createClientPane.add(paymentLabel);
-        createClientPane.add(paymentInput);
+        createClientPanel.add(nameLabel);
+        createClientPanel.add(nameInput);
+        createClientPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
+        createClientPanel.add(addressLabel);
+        createClientPanel.add(addressInput);
+        createClientPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
+        createClientPanel.add(phoneLabel);
+        createClientPanel.add(phoneInput);
+        createClientPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
+        createClientPanel.add(paymentLabel);
+        createClientPanel.add(paymentInput);
 
-        // Create client pane - button
-        createClientPane.add(Box.createRigidArea(new Dimension(1000, 80)));
+        // Create client panel - button
+        createClientPanel.add(Box.createRigidArea(new Dimension(1000, 80)));
         JButton saveButton = new JButton("Adicionar");
         saveButton.setPreferredSize(new Dimension(200, 30));
-        createClientPane.add(saveButton);
+        createClientPanel.add(saveButton);
 
         // Edit client pane - title
-        editClientPane.add(Box.createRigidArea(new Dimension(1000, 10)));
-        editClientPane.add(new JLabel("Editar cliente"));
-        editClientPane.add(Box.createRigidArea(new Dimension(1000, 20)));
+        editClientPanel.add(Box.createRigidArea(new Dimension(1000, 10)));
+        editClientPanel.add(new JLabel("Editar cliente"));
+        editClientPanel.add(Box.createRigidArea(new Dimension(1000, 20)));
 
-        // Edit client pane - dropdown
+        // Edit client panel - dropdown
         JLabel clientLabel = new JLabel("Cliente:");
         clientLabel.setPreferredSize(new Dimension(100, 30));
-        editClientPane.add(clientLabel);
+        editClientPanel.add(clientLabel);
         String[] fakeChoices = {"a", "b", "c"};
         JComboBox<String> dropdown = new JComboBox<String>(fakeChoices);
         dropdown.setPreferredSize(new Dimension(700, 30));
-        editClientPane.add(dropdown);
-        editClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
+        editClientPanel.add(dropdown);
+        editClientPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
 
-        // Edit client pane - inputs
+        // Edit client panel - inputs
         JLabel nameEditLabel = new JLabel("Nome:");
         nameEditLabel.setPreferredSize(new Dimension(100, 30));
         JTextField nameEditInput = new JTextField();
@@ -105,30 +101,33 @@ public class ClientePanel extends JPanel {
         JTextField paymentEditInput = new JTextField();
         paymentEditInput.setPreferredSize(new Dimension(700, 30));
 
-        editClientPane.add(nameEditLabel);
-        editClientPane.add(nameEditInput);
-        editClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
-        editClientPane.add(addressEditLabel);
-        editClientPane.add(addressEditInput);
-        editClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
-        editClientPane.add(phoneEditLabel);
-        editClientPane.add(phoneEditInput);
-        editClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
-        editClientPane.add(paymentEditLabel);
-        editClientPane.add(paymentEditInput);
+        editClientPanel.add(nameEditLabel);
+        editClientPanel.add(nameEditInput);
+        editClientPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
+        editClientPanel.add(addressEditLabel);
+        editClientPanel.add(addressEditInput);
+        editClientPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
+        editClientPanel.add(phoneEditLabel);
+        editClientPanel.add(phoneEditInput);
+        editClientPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
+        editClientPanel.add(paymentEditLabel);
+        editClientPanel.add(paymentEditInput);
 
-        // Edit client pane - button
-        editClientPane.add(Box.createRigidArea(new Dimension(1000, 30)));
+        // Edit client panel - buttons
+        editClientPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
         JButton editButton = new JButton("Editar");
         editButton.setPreferredSize(new Dimension(200, 30));
-        editClientPane.add(editButton);
+        JButton deleteButton = new JButton("Deletar");
+        deleteButton.setPreferredSize(new Dimension(200, 30));
+        editClientPanel.add(editButton);
+        editClientPanel.add(deleteButton);
 
-        // List clients pane - title
-        listClientsPane.add(Box.createRigidArea(new Dimension(1000, 10)));
-        listClientsPane.add(new JLabel("Listar clientes"));
-        listClientsPane.add(Box.createRigidArea(new Dimension(1000, 20)));
+        // List clients panel - title
+        listClientsPanel.add(Box.createRigidArea(new Dimension(1000, 10)));
+        listClientsPanel.add(new JLabel("Listar clientes"));
+        listClientsPanel.add(Box.createRigidArea(new Dimension(1000, 20)));
 
-        // List clients pane - list
+        // List clients panel - list
         String[] fakeClients = {"Dion", "Eurico"};
         JPanel clientsListPanel = new JPanel();
         clientsListPanel.setLayout(new BoxLayout(clientsListPanel, BoxLayout.PAGE_AXIS));
@@ -140,7 +139,7 @@ public class ClientePanel extends JPanel {
         clientsListScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         clientsListScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         clientsListScroll.setPreferredSize(new Dimension(800, 350));
-        listClientsPane.add(clientsListScroll);
+        listClientsPanel.add(clientsListScroll);
 
         this.add(tabbedPane);
     }
