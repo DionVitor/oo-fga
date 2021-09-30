d92ba0bbf7504e22137850701d1864e34c9f43c4
dionvitor <dionvictor11@gmail.com>
2021-09-30 13:16:42 -0300

    Adicionado interface de edição, deleção e listagem de cardápios.

--- src/tp4/gui/MenuPanel.java
+++ src/tp4/gui/MenuPanel.java
@@ -1,12 +1,21 @@
 package tp4.gui;
 
+import tp4.domain.Cardapio;
+import tp4.domain.Cliente;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 
 public class MenuPanel extends JPanel {
+    private final ArrayList<Cardapio> menus;
+
     public MenuPanel() {
         super(false);
+        this.menus = new ArrayList<Cardapio>();
+        final JComboBox<String> dropdown = new JComboBox<>();
+        JPanel menuListPanel = new JPanel();
 
         // Title
         this.add(Box.createRigidArea(new Dimension(1000, 8)));
@@ -55,6 +64,70 @@ public class MenuPanel extends JPanel {
         createMenuButton.setPreferredSize(new Dimension(200, 30));
         createMenuPanel.add(createMenuButton);
 
+        // Edit menu panel - title
+        editMenuPanel.add(Box.createRigidArea(new Dimension(1000, 10)));
+        editMenuPanel.add(new JLabel("Editar cardápio"));
+        editMenuPanel.add(Box.createRigidArea(new Dimension(1000, 20)));
+
+        // Edit menu panel - dropdown
+        JLabel menuLabel = new JLabel("Cardápio:");
+        menuLabel.setPreferredSize(new Dimension(100, 30));
+        editMenuPanel.add(menuLabel);
+        for (Cardapio menu : this.menus) {
+            dropdown.addItem(menu.getTipo());
+        }
+        dropdown.setPreferredSize(new Dimension(700, 30));
+        editMenuPanel.add(dropdown);
+        editMenuPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
+
+        // Edit menu panel - inputs
+        JLabel typeEditLabel = new JLabel("Tipo:");
+        typeEditLabel.setPreferredSize(new Dimension(100, 30));
+        JTextField typeEditInput = new JTextField();
+        typeEditInput.setPreferredSize(new Dimension(700, 30));
+
+        editMenuPanel.add(typeEditLabel);
+        editMenuPanel.add(typeEditInput);
+        editMenuPanel.add(Box.createRigidArea(new Dimension(1000, 30)));
+
+        // Edit menu panel - buttons
+        JButton editButton = new JButton("Editar");
+        JButton deleteButton = new JButton("Deletar");
+        editButton.setPreferredSize(new Dimension(200, 30));
+        deleteButton.setPreferredSize(new Dimension(200, 30));
+
+        editMenuPanel.add(Box.createRigidArea(new Dimension(1000, 180)));
+        editMenuPanel.add(editButton);
+        editMenuPanel.add(deleteButton);
+
+        // List menu panel - title
+        listMenuPanel.add(Box.createRigidArea(new Dimension(1000, 10)));
+        listMenuPanel.add(new JLabel("Listar clientes"));
+        listMenuPanel.add(Box.createRigidArea(new Dimension(1000, 20)));
+
+        // List menu panel - list
+        ArrayList<String> menuTypes = this.getMenuTypes(this.menus);
+        menuListPanel.setLayout(new BoxLayout(menuListPanel, BoxLayout.PAGE_AXIS));
+        for (String menuType : menuTypes) {
+            menuListPanel.add(new JLabel("- " + menuType));
+            menuListPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        }
+        JScrollPane clientsListScroll = new JScrollPane(menuListPanel);
+        clientsListScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        clientsListScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        clientsListScroll.setPreferredSize(new Dimension(800, 350));
+        listMenuPanel.add(clientsListScroll);
+
         this.add(tabbedPane);
     }
+
+    private ArrayList<String> getMenuTypes(ArrayList<Cardapio> menus) {
+        ArrayList<String> menuTypes = new ArrayList<>();
+        for (Cardapio menu : menus) {
+            menuTypes.add(menu.getTipo());
+        }
+        return menuTypes;
+    }
+
+
 }
