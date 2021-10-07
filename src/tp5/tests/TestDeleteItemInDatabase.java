675769e5d11f4ee9fa1202922e2628f7f32be3c0
dionvitor <dionvictor11@gmail.com>
2021-10-07 10:34:15 -0300

    Adicionado testes de deleção de itens no banco de dados.

--- src/tp5/tests/TestDeleteItemInDatabase.java
+++ src/tp5/tests/TestDeleteItemInDatabase.java
@@ -4,20 +4,23 @@ import tp5.domain.Cardapio;
 import tp5.domain.Cliente;
 import tp5.domain.Produto;
 import tp5.domain.Venda;
-import tp5.gui.*;
 
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import tp5.gui.ClientePanel;
+import tp5.gui.MenuPanel;
+import tp5.gui.ProdutoPanel;
+import tp5.gui.VendaPanel;
 
 
-public class TestCreateItemInDatabase {
+public class TestDeleteItemInDatabase {
     private final ArrayList<Cardapio> arrayMenu;
     private final ArrayList<Cliente> arrayClients;
     private final ArrayList<Venda> arraySales;
     private final ArrayList<Produto> arrayProducts;
 
-    public TestCreateItemInDatabase() {
+    public TestDeleteItemInDatabase() {
         this.arrayMenu = new ArrayList<>();
         this.arrayClients = new ArrayList<>();
         this.arraySales = new ArrayList<>();
@@ -25,26 +28,30 @@ public class TestCreateItemInDatabase {
     }
 
     @Test
-    void testCreateSaleInDatabase() {
+    void testDeleteSaleInDatabase() {
         VendaPanel.registerSale(this.arraySales, "Dion", "Maça", "10");
-        assertEquals(this.arraySales.toArray().length, 1);
+        VendaPanel.deleteSale(this.arraySales, "Dion", "Maça", "10");
+        assertEquals(this.arraySales.toArray().length, 0);
     }
 
     @Test
-    void testCreateClientInDatabase() {
+    void testDeleteClientInDatabase() {
         ClientePanel.registerClient(this.arrayClients, "Dion", "Gama", "61-923434220", "Cartão");
-        assertEquals(this.arrayClients.toArray().length, 1);
+        ClientePanel.deleteClient(this.arrayClients, "Dion");
+        assertEquals(this.arrayClients.toArray().length, 0);
     }
 
     @Test
-    void testCreateProductInDatabase() {
+    void testDeleteProductInDatabase() {
         ProdutoPanel.registerProduct(this.arrayProducts, "Maça", "4", "Gostosa fruta.", "5");
-        assertEquals(this.arrayProducts.toArray().length, 1);
+        ProdutoPanel.deleteProduct(this.arrayProducts, "Maça");
+        assertEquals(this.arrayProducts.toArray().length, 0);
     }
 
     @Test
-    void testCreateMenuInDatabase() {
+    void testDeleteMenuInDatabase() {
         MenuPanel.registerMenu(this.arrayMenu, "Frutas");
-        assertEquals(this.arrayMenu.toArray().length, 1);
+        MenuPanel.deleteMenu(this.arrayMenu, "Frutas");
+        assertEquals(this.arrayMenu.toArray().length, 0);
     }
 }
